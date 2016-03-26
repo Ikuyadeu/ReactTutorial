235f835ea442db05315cf3f1a83051b9f06ef4cd
Ikuyadeu <ikuyadeu@yahoo.co.jp>
2016-03-26 17:02:02 +0900

    Hook up the data model

---
+++ src/tutorial6.js
@@ -0,0 +1,30 @@
+// tutorial6.js
+var Comment = React.createClass({
+  render: function() {
+    return (
+      <div className="comment">
+        <h2 className="commentAuthor">
+          {this.props.author}
+        </h2>
+        {marked(this.props.children.toString())}
+      </div>
+    );
+  }
+});
+
+
+var CommentList = React.createClass({
+  render: function() {
+    return (
+      <div className="commentList">
+        <Comment author="Pete Hunt">This is one comment</Comment>
+        <Comment author="Jordan Walke">This is *another* comment</Comment>
+      </div>
+    );
+  }
+});
+
+ReactDOM.render(
+  <CommentList />,
+  document.getElementById('content')
+);
