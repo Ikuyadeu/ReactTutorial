8561c8ffe28e5b5439b129eab188a5011932e88e
Ikuyadeu <ikuyadeu@yahoo.co.jp>
2016-04-03 18:26:22 +0900

    add ajax

---
+++ src/tutorial16.js
@@ -0,0 +1,103 @@
+var data = [
+  {id: 1, "author": "Pete Hunt", "text": "This is one comment"},
+  {id: 2, "author": "Jordan Walke", "text": "This is *another* comment"}
+];
+
+var Comment = React.createClass({
+	rawMarkup: function() {
+		var rawMarkup = marked(this.props.children.toString(), {sanitize: true});
+		return { __html: rawMarkup };
+	},
+
+  render: function() {
+    return (
+      <div className="comment">
+        <h2 className="commentAuthor">
+          {this.props.author}
+        </h2>
+        <span dangerouslySetInnerHTML={this.rawMarkup()} />
+      </div>
+    );
+  }
+});
+
+
+var CommentList = React.createClass({
+  render: function() {
+    return (
+      <div className="commentList">
+        <Comment author="Pete Hunt">This is one comment</Comment>
+        <Comment author="Jordan Walke">This is *another* comment</Comment>
+      </div>
+    );
+  }
+});
+
+var CommentForm = React.createClass({
+  getInitialState: function(){
+    return {author: '', text: ''};
+  },
+  handleAuthorChange: function(e) {
+    this.setState({author: e.target.value});
+  },
+  handleTextChange: function(e) {
+    this.setState({text: e.target.value})
+  },
+  render:function() {
+    return(
+      <form className="commentForm">
+        <input
+          type="text"
+          placeholder="Your name"
+          value={this.state.author}
+          onChange={this.handleAuthorChange}
+        />
+        <input
+          type="text"
+          placeholder="Say something..."
+          value={this.state.author}
+          onChange={this.handleAuthorChange}
+        />
+        <input type="submit" placeholder="Post" />
+      </form>
+    )
+  }
+});
+
+var CommentBox = React.createClass({
+  loadCommentsFromServer: function(){
+    $.ajax({
+      url: this.props.url,
+      dataType: 'json',
+      cache: false,
+      success: function(data) {
+        this.setState({data: data});
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error(this.props.url, status, err.toString());
+      }.bind(this)
+    });
+  },
+  getInitialState: function() {
+    return {data: []};
+  },
+	componentDidMount: function() {
+	   this.loadCommentsFromServer();
+     setInterval(this.loadCommentsFromServer, this.props.pollInterval);
+	},
+  render: function() {
+    return (
+      <div className="commentBox">
+        <h1>Comments</h1>
+        <CommentList data={this.state.data}/>
+        <CommentForm />
+      </div>
+    );
+  }
+});
+
+
+ReactDOM.render(
+  <CommentBox url="/api/comments" pollInterval={2000} />,
+  document.getElementById('content')
+);
